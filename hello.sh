61803fc2b4ca0cd7a5167aac829909fd5c6f838e
venkat1211 <50703194+venkat1211@users.noreply.github.com>
2021-02-14 00:14:32 +0530

    test

---
+++ hello.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+sudo yum -y install git
+sudo yum -y install maven
+
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sudo sh -c 'echo -e "[azure-cli]
+name=Azure CLI
+baseurl=https://packages.microsoft.com/yumrepos/azure-cli
+enabled=1
+gpgcheck=1
+gpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/azure-cli.repo'
+sudo yum -y install azure-cli
+
+sudo sh -c 'echo -e "[Kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+enabled=1
+gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg" > /etc/yum.repos.d/kubernetes.repo'
+sudo yum install -y kubectl
+
+sudo yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+sudo yum -y install jq
